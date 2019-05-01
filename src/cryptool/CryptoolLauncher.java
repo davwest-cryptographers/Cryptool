ecec782619d5db91ee2699420614e66ddd4713ee
Simmonds7 <kristofer.simmonds@gmail.com>
2019-05-01 13:45:16 -0500

    Minor changes, NEED TO USE AN ARRAY SOMEWHERE

--- src/cryptool/CryptoolLauncher.java
+++ src/cryptool/CryptoolLauncher.java
@@ -11,36 +11,43 @@ static Scanner keyboard = new Scanner(System.in);
 
      */
     public static void main(String[] args) throws Exception {
-        String resultText;
-        System.out.println("Welcome to Cryptool, Would you like to ENCRYPT or DECRYPT?");
-        String cypherChoice = keyboard.nextLine();
-        System.out.println("Enter File Path to begin.");
-        String filePath = keyboard.nextLine();
-        System.out.println("Now input your shift key. Integer (0-999999999) only.");
-        int encryptionKey = keyboard.nextInt();
+        System.out.println("Welcome to Cryptool. Created by Kris S, and Amia C.");
+        int runtime = 1;
+        String cypherChoice;
+        while(runtime != 0) {
+            String resultText;
+            System.out.println("Would you like to ENCRYPT or DECRYPT? Or Enter something else to exit.");
+            cypherChoice = keyboard.next();
+            System.out.println("Enter File Path to begin.");
+            String filePath = keyboard.next();
+            System.out.println("Now input your shift key. Integer (0-999999999) only.");
+            int encryptionKey = keyboard.nextInt();
 
-        String ImportedText = ReadFromTxtFile.getFile(filePath);
-        switch (cypherChoice.toLowerCase()) {
-            case "encrypt":
-                resultText = Encryptor.encryptText(ImportedText, encryptionKey);
-                try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                        new FileOutputStream("ResultFile.txt"), "utf-8"))) {
-                    writer.write(resultText);
-                    System.out.println("File written: ResultFile.txt");
-                }
-                break;
-            case "decrypt":
-                resultText = Encryptor.decryptText(ImportedText, encryptionKey);
-                try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                        new FileOutputStream("ResultFile.txt"), "utf-8"))) {
-                    writer.write(resultText);
-                    System.out.println("File written: ResultFile.txt");
-                    // DatabaseConn.DatabaseUpdate(filePath, encryptionKey, resultText, cypherChoice);
-                }
-                break;
-            default:
-                System.out.println("Command: " + cypherChoice + "\nNot Recognized, Program Exit");
-                break;
+            switch (cypherChoice.toLowerCase()) {
+                case "encrypt":
+                    String ImportedText = ReadFromTxtFile.getFile(filePath);
+                    resultText = Encryptor.encryptText(ImportedText, encryptionKey);
+                    try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                            new FileOutputStream("ResultFile.txt"), "utf-8"))) {
+                        writer.write(resultText);
+                        System.out.println("File written: ResultFile.txt");
+                    }
+                    break;
+                case "decrypt":
+                    ImportedText = ReadFromTxtFile.getFile(filePath);
+                    resultText = Encryptor.decryptText(ImportedText, encryptionKey);
+                    try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                            new FileOutputStream("ResultFile.txt"), "utf-8"))) {
+                        writer.write(resultText);
+                        System.out.println("File written: ResultFile.txt");
+                        // DatabaseConn.DatabaseUpdate(filePath, encryptionKey, resultText, cypherChoice);
+                    }
+                    break;
+                default:
+                    System.out.println("Exiting program.");
+                    runtime = 0;
+                    break;
+            }
         }
-        }
+    }
     }
